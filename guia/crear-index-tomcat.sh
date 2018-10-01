21f46ee465c56fb98a605667811337367d42c73e
Carlos G. Consis <cgomez@consisint.com>
2018-10-01 14:04:18 -0400

    Crear Index para Tomcat

--- guia/crear-index-tomcat.sh
+++ guia/crear-index-tomcat.sh
@@ -7,7 +7,7 @@ echo -e "<html>
 <STYLE>
 <!--H1 {font-family:Tahoma,Arial,sans-serif;color:white;background-color:#525D76;font-size:22px;} H2 {font-family:Tahoma,Arial,sans-serif;color:white;background-color:#525D76;font-size:16px;} H3 {font-family:Tahoma,Arial,sans-serif;color:white;background-color:#525D76;font-size:14px;} BODY {font-family:Tahoma,Arial,sans-serif;color:black;background-color:white;} B {font-family:Tahoma,Arial,sans-serif;color:white;background-color:#525D76;} P {font-family:Tahoma,Arial,sans-serif;background:white;color:black;font-size:12px;}A {color : black;}A.name {color : black;}HR {color : #525D76;}--></STYLE> </head>
 <body>
-<img src=\"consis.png\" alt=\"width=\" height=\"60\" border=\"0\" />
+<img src=\"Dominio.png\" alt=\"width=\" height=\"60\" border=\"0\" />
 <h1>APPS / Carlos Gomez ---> </h1>
 <HR size=\"1\" noshade=\"noshade\">" > /opt/apache-tomcat-9.0.12/webapps/apps/index.html
 
@@ -29,7 +29,7 @@ do
 		COLOR=1
 	fi
                 echo -e "<td align=\"left\">&nbsp;&nbsp;" >> /opt/apache-tomcat-9.0.12/webapps/apps/index.html
-		echo -e "<a href=\"$i/\"><tt>$i/</tt></a></td>" >> /opt/apache-tomcat-9.0.12/webapps/apps/index.html
+		echo -e "<a href=\"$i\"><tt>$i/</tt></a></td>" >> /opt/apache-tomcat-9.0.12/webapps/apps/index.html
                 echo -e "</tr>" >> /opt/apache-tomcat-9.0.12/webapps/apps/index.html
 done
 		echo -e "</table>" >> /opt/apache-tomcat-9.0.12/webapps/apps/index.html
